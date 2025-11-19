53bba2e9e936430c171283b58c1e689ed22a63b5
pollob debnath <pullobdebnath@gmail.com>
2025-11-19 14:48:01 +0600

    buttons  clickable

--- connector.js
+++ connector.js
@@ -1,1 +1,20 @@
-//Nature
+const buttons = document.querySelectorAll(".btn-div .btn");
+const cards = document.querySelectorAll(".card");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", ()=>{
+    const filter = btn.textContent.toLowerCase();
+
+    cards.forEach((card) => {
+      if (filter === "all") {
+        card.classList.remove("hidden");
+      } else {
+        if (card.classList.contains(filter)) {
+          card.classList.remove("hidden");
+        } else {
+          card.classList.add("hidden");
+        }
+      }
+    });
+  });
+});
